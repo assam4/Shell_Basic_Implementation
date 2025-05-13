8e6cefb3c2da00e4d6ca319cca65f4515769d28b
assam4 <samaslanyan3@gmail.com>
2025-05-14 00:05:14 +0400

    add execute pipe

--- runtime/executor.c
+++ runtime/executor.c
@@ -34,6 +34,49 @@ static bool	execute_subshell(t_ast_node *node, char **env)
 		return (false);
 }
 
+static bool	stream_change(pid_t *process_id, t_ast_node *left
+			, t_ast_node *right, char **env)
+{
+	int	fd[2];
+
+	if (pipe(fd) < 0)
+		return (false);
+	process_id[0] = fork();
+	if (process_id[0] == -1)
+		return (close(fd[0]), close(fd[1]), false);
+	if (!process_id[0])
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		exit(!execute_node(left, env));
+	}
+	process_id[1] = fork();
+	if (process_id[1] == -1)
+		return (close(fd[0]), close(fd[1]), false);
+	if (!process_id[1])
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		exit(!execute_node(right, env));
+	}
+	return (close(fd[0]), close(fd[1]), true);
+}
+
+static bool	execute_pipe(t_ast_node *left, t_ast_node *right, char **env)
+{
+	pid_t	process_id[2];
+	int		status[2];
+
+	if (!stream_change(process_id, left, right, env))
+		return (false);
+	waitpid(process_id[0], status, 0);
+	waitpid(process_id[1], status + 1, 0);
+	return ((WIFEXITED(status[0]) && !WEXITSTATUS(status[0]))
+		&& (WIFEXITED(status[1]) && !WEXITSTATUS(status[1])));
+}
+
 bool	execute_node(t_ast_node *node, char **env)
 {
 	if (!node)
