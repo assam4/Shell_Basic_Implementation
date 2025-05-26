2762191028bd7c1ac68c638ae2d8852a96603f0a
assam4 <samaslanyan3@gmail.com>
2025-05-26 15:56:39 +0400

    optimize expantion.c

--- runtime/expantion.c
+++ runtime/expantion.c
@@ -6,47 +6,37 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 19:17:59 by aadyan            #+#    #+#             */
-/*   Updated: 2025/05/24 23:41:15 by aadyan           ###   ########.fr       */
+/*   Updated: 2025/05/26 15:55:27 by saslanya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 
-char	*ft_join_with_free(char *str1, char *str2, char sep)
+static bool	add_to_str(char **dest, char **src)
 {
-	int		i;
-	int		j;
-	char	*str;
+	char	*new_str;
 
-	str = malloc(sizeof(char) * (ft_strlen(str1) + ft_strlen(str2) + 2));
-	if (!str)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (str1 && str1[i])
-		str[j++] = str1[i++];
-	str[j++] = sep;
-	i = 0;
-	while (str2 && str2[i])
-		str[j++] = str2[i++];
-	str[j] = '\0';
-	free(str1);
-	return (str);
+	if (!*src)
+		return (free(*dest), *dest = NULL, false);
+	new_str = ft_calloc(ft_strlen(*dest) + ft_strlen(*src) + 2, sizeof(char));
+	if (!new_str)
+		return (free(*dest), *dest = NULL, free(*src), *src = NULL, false);
+	ft_strlcpy(new_str, *dest, ft_strlen(*dest) + 1);
+	new_str[ft_strlen(new_str)] = ' ';
+	ft_strlcpy(new_str + ft_strlen(new_str), *src, ft_strlen(*src) + 1);
+	free(*dest);
+	free(*src);
+	*src = NULL;
+	*dest = new_str;
+	return (true);
 }
 
-static void	free_split(char ***s)
+static void	smart_free_split(char ***s, int i)
 {
-	size_t	i;
-
-	i = 0;
-	while ((*s)[i])
-	{
+	while (i--)
 		free((*s)[i]);
-		++i;
-	}
 	free(*s);
 	*s = NULL;
-	return ;
 }
 
 static char	*expand_single_wildcard(char *token)
@@ -76,20 +66,23 @@ char	*expand_wildcard(char *cmd)
 	int		i;
 
 	splitted = ft_split(cmd, ' ');
+	i = 0;
 	if (!splitted)
 		return (NULL);
-	all_expanded = ft_strdup(splitted[0]);
-	i = 1;
+	all_expanded = splitted[i];
+	splitted[i++] = NULL;
 	while (splitted[i])
 	{
 		if (ft_strchr(splitted[i], '*'))
+		{
 			expanded = expand_single_wildcard(splitted[i]);
+			add_to_str(&all_expanded, &expanded);
+		}
 		else
-			expanded = ft_strdup(splitted[i]);
-		all_expanded = ft_join_with_free(all_expanded, expanded, ' ');
-		free(expanded);
-		i++;
+			add_to_str(&all_expanded, &(splitted[i]));
+		if (!all_expanded)
+			break ;
+		++i;
 	}
-	free_split(&splitted);
-	return (all_expanded);
+	return (smart_free_split(&splitted, i), all_expanded);
 }
