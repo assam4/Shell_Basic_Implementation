99f31a3631a09dabbb45d6f81b785b8493b42bed
AndranikAdyan <andranikadyan@gmail.com>
2025-06-03 15:32:34 +0400

    fix echo flag checking

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,24 +6,39 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 14:24:44 by aadyan            #+#    #+#             */
-/*   Updated: 2025/06/03 00:50:33 by aadyan           ###   ########.fr       */
+/*   Updated: 2025/06/03 15:32:26 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
+static void	check_flag(t_list **cmd, int *new_line)
+{
+	int	i;
+
+	while (ft_strncmp(((t_token *)(*cmd)->content)->word, "-n", 2) == 0)
+	{
+		i = 1;
+		while (((t_token *)(*cmd)->content)->word[i] &&
+				((t_token *)(*cmd)->content)->word[i] == 'n')
+			i++;
+		if (!((t_token *)(*cmd)->content)->word[i])
+		{
+			*new_line = 0;
+			(*cmd) = (*cmd)->next;
+		}
+	}
+}
+
 bool	echo(t_list *cmd)
 {
-	int		new_line;
+	int	new_line;
 
 	new_line = 1;
 	cmd = cmd->next;
-	if (ft_strncmp(((t_token *)cmd->content)->word, "-n",
-			ft_strlen(((t_token *)cmd->content)->word) + 1) == 0)
-	{
-		new_line = 0;
-		cmd = cmd->next;
-	}
+	if (!cmd)
+		return (ft_putchar_fd('\n', 1), true);
+	check_flag(&cmd, &new_line);
 	while (cmd)
 	{
 		if (!((t_token *)cmd->content)->word)
